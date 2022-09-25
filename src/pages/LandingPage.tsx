ce1ed147c81ee66ac90e6d58f273e40c59d6034d
yujiawei427 <yujiawei427@gmail.com>
2022-09-25 23:57:14 +1000

    LandingPage

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,15 +1,32 @@
-import { withRouter } from "react-router";
+import {
+  Link,
+  withRouter
+} from 'react-router-dom';
 import { Container } from "semantic-ui-react";
 import './LandingPage/LandingPage.less';
+import landingPageImage from '../image/landingPageImage.svg'
 
 const LandingPage: React.FunctionComponent = () => {
-  
+
   return (
-    <div>
-      <p className='header'>Imagine if</p>
-      <p className='highlight'>Snapchat</p>
-      <p>had events.</p>
-    </div> 
+    <div className='container'>
+      <div className='header-box'>
+        <div className='header'>
+          <p>Imagine if</p>
+          <p className='highlight'>Snapchat</p>
+          <p>had events.</p>
+        </div>
+        <div className='sub-header'>
+          <p>Easily host and share events with your friends across any social media.</p>
+        </div>
+      </div>
+      <div>
+        <img src={landingPageImage} className='example-img' />
+      </div>
+      <Link to='/create'>
+        <button className='create-event-button'>🎉 Create my event</button>
+      </Link>
+    </div>
   )
 };
 
